e4f2f9962de9a4404943b5982bc243334f55b404
Kayoung <rkdkdudjd@gmail.com>
2022-09-08 03:35:51 +0900

    ConnectionMaker 를 멤버변수에 추가
    * 하지만 두 가지 문제 발생
    * SimpleConnectionMaker의 메소드의 이름이 다르면 UserDao의 코드를 수정해야한다.
    * Connection Maker 역할을 하는 클래스의 이름이 달라지면 UserDao의 코드를 수정해야한다.

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -5,9 +5,12 @@ import springbook.user.domain.User;
 import java.sql.*;
 
 public abstract class UserDao {
-    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+    private SimpleConnectionMaker simpleConnectionMaker;
+    public UserDao() {
+        simpleConnectionMaker = new SimpleConnectionMaker();
+    }
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.getConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -20,7 +23,7 @@ public abstract class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.getConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
 
