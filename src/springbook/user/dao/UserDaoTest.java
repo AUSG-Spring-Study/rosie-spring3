0755c4ce161fd36cba93e2cb85836165ede241e4
Kayoung <rkdkdudjd@gmail.com>
2022-09-08 16:13:24 +0900

    UserDaoTest는 UserDao 생성에 관여하지 않는다
    * DaoFactory 클래스에서 UserDao를 생성하는 책임을 갖는다.
    * UserDaoTest는 UserDao의 기능을 테스트하는 책임만 갖게함!

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -6,8 +6,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
 
         User user = dao.get("rosie");
         System.out.println(user.getId() + " 조회 성공"); // rosie 조회 성공
