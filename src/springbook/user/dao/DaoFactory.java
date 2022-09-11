b21436036c53475470191158ef454de3cb4cdb60
Kayoung <rkdkdudjd@gmail.com>
2022-09-11 20:14:32 +0900

    CountingConnectionMaker - 커넥션의 개수를 세는 부가기능 추가

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -3,7 +3,7 @@ package springbook.user.dao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class DaoFactory {
     @Bean
     public UserDao userDao() { // 메서드의 이름이 빈의 이름이 된다.
