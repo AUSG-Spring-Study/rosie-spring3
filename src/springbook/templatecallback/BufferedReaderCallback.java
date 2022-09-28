8879e80d22221e76887276749ac46f94f6eabc04
Kayoung <rkdkdudjd@gmail.com>
2022-09-28 18:24:08 +0900

    템플릿/콜백 적용 예제 - (2) 템플릿/콜백으로 리팩토링

--- src/springbook/templatecallback/BufferedReaderCallback.java
+++ src/springbook/templatecallback/BufferedReaderCallback.java
@@ -1,2 +1,9 @@
-package springbook.templatecallback;public interface BufferedReaderCallback {
+package springbook.templatecallback;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public interface BufferedReaderCallback {
+    Integer doSomethingWithReader(BufferedReader br) throws IOException;
 }
