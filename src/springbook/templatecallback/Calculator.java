090395936e3bd7b95dc58fc55513d53aa752c47f
Kayoung <rkdkdudjd@gmail.com>
2022-09-28 22:47:41 +0900

    템플릿/콜백 적용 예제 - (4) File이 아닌 Line을 기준으로 전략 나누기

--- src/springbook/templatecallback/Calculator.java
+++ src/springbook/templatecallback/Calculator.java
@@ -25,34 +25,42 @@ public class Calculator {
         }
     }
 
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer res = initVal;
+            String line = null;
+            while((line = br.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+            }
+            return res;
+        } catch (IOException e) {
+            System.out.println(e);
+            throw e;
+        } finally {
+            br.close();
+        }
+    }
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer sum = 0;
-                String line = null;
-                while((line = br.readLine()) != null) {
-                    sum += Integer.valueOf(line);
-                }
-                return sum;
+        LineCallback sumLineCallback = new LineCallback() {
+            @Override
+            public Integer doSomethingWithLine(String line, Integer value) {
+                return value + Integer.valueOf(line);
             }
         };
-        return fileReadTemplate(filepath, sumCallback);
+        return lineReadTemplate(filepath, sumLineCallback, 0);
     }
 
     public Integer calcMultiply(String filePath) throws IOException{
-        BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
+        LineCallback multiplyLineCallback = new LineCallback() {
             @Override
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer multiply = 1;
-                String line = null;
-                while((line = br.readLine()) != null) {
-                    multiply *= Integer.valueOf(line);
-                }
-                return multiply;
+            public Integer doSomethingWithLine(String line, Integer value) {
+                return value * Integer.valueOf(line);
             }
         };
 
-        return fileReadTemplate(filePath, multiplyCallback);
+        return lineReadTemplate(filePath, multiplyLineCallback, 1);
     }
 
 }
